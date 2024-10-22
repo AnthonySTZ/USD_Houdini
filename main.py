be8109fffe4e8351dca6f70b010b84b5f1d34bcd
AnthonySTZ <anthonycandy2309@gmail.com>
2024-10-22 13:44:00 +0200

    remove all margins

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from PySide2 import QtGui, QtCore, QtWidgets
 from pxr import Usd, UsdUtils, Sdf
 from pxr.Usdviewq.stageView import StageView
 
-class Widget(QtWidgets.QWidget):
+class PreviewUSDWidget(QtWidgets.QWidget):
     def __init__(self, stage=None):
-        super(Widget, self).__init__()
+        super(PreviewUSDWidget, self).__init__()
         self.model = StageView.DefaultDataModel()
         
         self.view = StageView(dataModel=self.model)
@@ -37,12 +37,27 @@ class Window(QtWidgets.QMainWindow):
         central_widget = QtWidgets.QWidget(self)
         self.setCentralWidget(central_widget)
         
-        layout = QtWidgets.QVBoxLayout(central_widget)
-        layout.setContentsMargins(0, 0, 0, 0)
+        main_layout = QtWidgets.QHBoxLayout(central_widget)
+        main_layout.setContentsMargins(0, 0, 0, 0)
+        left_widget = QtWidgets.QWidget()
+        right_widget = QtWidgets.QWidget()
+        main_layout.addWidget(left_widget)
+        main_layout.addWidget(right_widget)
+        
+        left_layout = QtWidgets.QVBoxLayout(left_widget)
+        left_layout.setContentsMargins(0, 0, 0, 0)
         
         buttons_widget = QtWidgets.QWidget()
         button_widget_layout = QtWidgets.QVBoxLayout(buttons_widget)
-        button_widget_layout.setContentsMargins(50, 10, 50, 0)
+        button_widget_layout.setContentsMargins(0, 0, 0, 0)
+
+        self.folder_path_te = QtWidgets.QTextEdit()
+        self.folder_path_te.setReadOnly(True)
+        self.folder_path_te.setMaximumHeight(30)
+        self.folder_path_te.setAlignment(QtCore.Qt.AlignBottom)
+        self.folder_path_te.setContentsMargins(0, 0, 0, 0)
+        self.folder_path_te.setPlaceholderText("folder path...")
+        button_widget_layout.addWidget(self.folder_path_te)
 
         self.select_folder_btn = QtWidgets.QPushButton("Select USD Folder")
         button_widget_layout.addWidget(self.select_folder_btn)
@@ -51,15 +66,21 @@ class Window(QtWidgets.QMainWindow):
         self.import_btn = QtWidgets.QPushButton("Import To Houdini")
         button_widget_layout.addWidget(self.import_btn)
 
-        layout.addWidget(buttons_widget)
+        left_layout.addWidget(buttons_widget)
 
         file_list_widget = QtWidgets.QWidget()
         file_list_layout = QtWidgets.QVBoxLayout(file_list_widget)
-        file_list_layout.setContentsMargins(50, 10, 50, 10)
+        file_list_layout.setContentsMargins(0, 0, 0, 0)
 
         self.file_list = QtWidgets.QListWidget()
         file_list_layout.addWidget(self.file_list)
-        layout.addWidget(file_list_widget)
+        left_layout.addWidget(file_list_widget)
+
+
+        right_layout = QtWidgets.QVBoxLayout(right_widget)
+        right_layout.setContentsMargins(0, 0, 0, 0)
+        self.usd_tree = QtWidgets.QTreeWidget()
+        right_layout.addWidget(self.usd_tree)
         
         self.resize(QtCore.QSize(750, 750))
 
